b5758ca9e54a8fc6b2c3e8b3b1f9814e61a3707e
sabotuer99 <sabotuer99@gmail.com>
2016-11-22 13:43:17 -0700

    Completed the "PiMatch" example.  Notes:  Pay special attention to data types, and read over the requirements twice!

---
+++ InterviewPuzzles/src/puzzles/sonmez/pimatch/PiMatch.java
@@ -0,0 +1,24 @@
+package puzzles.sonmez.pimatch;
+
+public class PiMatch {
+
+	public static double score(long num) {
+		
+		if(num < 100){
+			return num - 314.0;
+		}
+		
+		double score = 0;
+		int count = 0;
+		
+		while(num >= 100){
+			long right = num % 1000L;
+			score += right - 314.0;
+			num /= 10;
+			count += 1;
+		}
+		
+		return score/count;
+	}
+
+}
---
+++ InterviewPuzzles/src/spec/sonmez/pimatch/PiMatchTests.java
@@ -0,0 +1,48 @@
+package spec.sonmez.pimatch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import puzzles.sonmez.pimatch.PiMatch;
+
+public class PiMatchTests {
+
+	@Test
+	public void Given_3149_GetCorrectCalc() {
+		long num = 3149;
+		
+		double score = PiMatch.score(num);
+		
+		assertEquals(-82.5, score, 0.0000001);
+	}
+	
+	@Test
+	public void Given_357878_GetCorrectCalc() {
+		long num = 3149;
+		
+		double score = PiMatch.score(num);
+		
+		assertEquals(-82.5, score, 0.0000001);
+	}
+	
+	@Test
+	public void Given_123456789012_GetCorrectCalc() {
+		long num = 123456789012L;
+		
+		double score = PiMatch.score(num);
+		
+		assertEquals(185.5, score, 0.0000001);
+	}
+	
+	@Test
+	public void Given_0_GetCorrectCalc() {
+		long num = 0;
+		
+		double score = PiMatch.score(num);
+		
+		assertEquals(-314.0, score, 0.0000001);
+	}
+	
+
+}
