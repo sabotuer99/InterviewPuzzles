56541f3ff0b16b052e5c18fca6280299528e8a14
sabotuer99 <sabotuer99@gmail.com>
2016-11-22 13:54:29 -0700

    skeleton code to convert decimal to binary

---
+++ InterviewPuzzles/src/puzzles/sonmez/bithackdecbin/DecToBin.java
@@ -0,0 +1,5 @@
+package puzzles.sonmez.bithackdecbin;
+
+public class DecToBin {
+
+}
---
+++ InterviewPuzzles/src/spec/sonmez/bithackdecbin/DecToBinTests.java
@@ -0,0 +1,14 @@
+package spec.sonmez.bithackdecbin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DecToBinTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
