e53730dbe89789547c19e94b5f099a4edce65786
sabotuer99 <sabotuer99@gmail.com>
2016-11-22 11:54:55 -0700

    added skeleton for WordScore example

---
+++ InterviewPuzzles/src/puzzles/sonmez/wordscore/WordScore.java
@@ -0,0 +1,5 @@
+package puzzles.sonmez.wordscore;
+
+public class WordScore {
+
+}
---
+++ InterviewPuzzles/src/spec/sonmez/wordscore/WordScoreTests.java
@@ -0,0 +1,14 @@
+package spec.sonmez.wordscore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WordScoreTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
