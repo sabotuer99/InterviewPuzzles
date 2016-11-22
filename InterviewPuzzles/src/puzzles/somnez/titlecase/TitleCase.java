a532c4777a0a3e26bbab6edeefbaf52dfbe382b7
sabotuer99 <sabotuer99@gmail.com>
2016-11-22 10:31:46 -0700

    Title Case Passed unit tests

--- InterviewPuzzles/src/puzzles/somnez/titlecase/TitleCase.java
+++ InterviewPuzzles/src/puzzles/somnez/titlecase/TitleCase.java
@@ -1,6 +1,5 @@
 package puzzles.somnez.titlecase;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,24 +10,49 @@ public class TitleCase {
 	
 	public static String toTitleCase(String title){
 		// Lowercase the title string
+		String title_lower = title.toLowerCase();
+		
 		// Split the title string by spaces
+		String[] tokens = title_lower.split(" ");
+		
 		// For i from 0 to # of words
+		for(int i = 0; i < tokens.length; i++){
 			// If first or last
+			if(i == 0 || i == tokens.length-1){
 				// Capitalize word
+				tokens[i] = capitalize(tokens[i]);
+			} else {		
 			// Else
 				// If list does not contains the word
+				if(lowers.indexOf(tokens[i]) == -1){
 					// Capitalize word
+					tokens[i] = capitalize(tokens[i]);
+				}					
+			}
+		}
 		
 		//Combine words into sentence
-		return "";
+		String result = join(tokens);
+		return result;
 	}
 	
 	private static String capitalize(String word){
 		
 		// Lowercase word
+		String result = word.toLowerCase();
 		// Uppercase first letter	
+		result = result.substring(0,1).toUpperCase() + result.substring(1);
 		
-		return word;
+		return result;
+	}
+	
+	private static String join(String[] tokens){
+		StringBuilder sb = new StringBuilder();
+		for(String token : tokens){
+			sb.append(token).append(" ");
+		}
+		
+		return sb.toString().trim();
 	}
 	
 }
