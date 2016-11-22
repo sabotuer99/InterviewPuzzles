5cb0d91f17c59d0a4e1260ac7fb263bdf5b9bf55
sabotuer99 <sabotuer99@gmail.com>
2016-11-22 12:00:17 -0700

    Added baseline unit tests

--- InterviewPuzzles/src/puzzles/sonmez/wordscore/WordScore.java
+++ InterviewPuzzles/src/puzzles/sonmez/wordscore/WordScore.java
@@ -2,4 +2,9 @@ package puzzles.sonmez.wordscore;
 
 public class WordScore {
 
+	public static int score(String word) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- InterviewPuzzles/src/spec/sonmez/wordscore/WordScoreTests.java
+++ InterviewPuzzles/src/spec/sonmez/wordscore/WordScoreTests.java
@@ -4,11 +4,44 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import puzzles.sonmez.wordscore.WordScore;
+
 public class WordScoreTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void GivenXRayMachine_ScoreIs20() {
+		String word = "XRay Machine";
+		
+		int score = WordScore.score(word);
+		
+		assertEquals(20, score);
+	}
+	
+	@Test
+	public void GivenJabbt_ScoreIs13() {
+		String word = "Jabbt";
+		
+		int score = WordScore.score(word);
+		
+		assertEquals(13, score);
+	}
+	
+	@Test
+	public void GivenEmptyString_ScoreIs0() {
+		String word = "";
+		
+		int score = WordScore.score(word);
+		
+		assertEquals(0, score);
+	}
+	
+	@Test
+	public void GivenNull_ScoreIs0() {
+		String word = null;
+		
+		int score = WordScore.score(word);
+		
+		assertEquals(0, score);
 	}
 
 }
