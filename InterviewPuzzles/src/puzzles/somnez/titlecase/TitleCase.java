be3356e3cbee9f977715b4fa11f3a9ecbe737051
sabotuer99 <sabotuer99@gmail.com>
2016-11-22 09:35:37 -0700

    Added initial setup for pluralsight problem with Title Case

---
+++ InterviewPuzzles/src/puzzles/somnez/titlecase/TitleCase.java
@@ -0,0 +1,34 @@
+package puzzles.somnez.titlecase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TitleCase {
+
+	private static List<String> lowers = 
+			Arrays.asList("a", "the", "to", "at", "in", "with", "and", "but", "or");
+	
+	public static String toTitleCase(String title){
+		// Lowercase the title string
+		// Split the title string by spaces
+		// For i from 0 to # of words
+			// If first or last
+				// Capitalize word
+			// Else
+				// If list does not contains the word
+					// Capitalize word
+		
+		//Combine words into sentence
+		return "";
+	}
+	
+	private static String capitalize(String word){
+		
+		// Lowercase word
+		// Uppercase first letter	
+		
+		return word;
+	}
+	
+}
---
+++ InterviewPuzzles/src/spec/TitleCaseTests.java
@@ -0,0 +1,15 @@
+package spec;
+
+import puzzles.somnez.titlecase.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TitleCaseTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
