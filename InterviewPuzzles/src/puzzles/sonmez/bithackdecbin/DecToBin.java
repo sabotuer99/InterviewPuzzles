917d78457e727f0e925a639919c844176d8530e9
sabotuer99 <sabotuer99@gmail.com>
2016-11-22 14:46:55 -0700

    Finished convertion code.  Tricky bug:  because the max value of integer is 2^31-1, checking for smaller powers of two needs to start at 2^30 (because 2^31 won't fit anyway...).

--- InterviewPuzzles/src/puzzles/sonmez/bithackdecbin/DecToBin.java
+++ InterviewPuzzles/src/puzzles/sonmez/bithackdecbin/DecToBin.java
@@ -2,4 +2,38 @@ package puzzles.sonmez.bithackdecbin;
 
 public class DecToBin {
 
+	public static String convert(int num) {
+		if(num == 0){
+			return "0";
+		}
+
+		/*
+		for(int j = 0; j <= 32; j++){
+			int a = 1 << j;
+			System.out.println(j + ": " + a);
+		}*/
+		
+		String on = "1";
+		String off = "0";
+
+		if(num < 0){
+			on = "0";
+			off = "1";
+			num = Math.abs(num + 1);
+		}
+		
+		String result = off;
+		
+		for(int i = 30; i >= 0; i--){
+			int power =  1 << i;
+			if(num >= power){
+				result += on;
+				num -= power;
+			} else {
+				result += off;
+			}
+		}
+		
+		return result.substring(result.indexOf('1'));		
+	}
 }
--- InterviewPuzzles/src/spec/sonmez/bithackdecbin/DecToBinTests.java
+++ InterviewPuzzles/src/spec/sonmez/bithackdecbin/DecToBinTests.java
@@ -4,11 +4,62 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import puzzles.sonmez.bithackdecbin.DecToBin;
+
 public class DecToBinTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Given2_Get10() {
+		int num = 2;
+		
+		String binary = DecToBin.convert(num);
+		
+		assertEquals("10", binary);
+	}
+	
+	@Test
+	public void Given3_Get11() {
+		int num = 3;
+		
+		String binary = DecToBin.convert(num);
+		
+		assertEquals("11", binary);
+	}
+	
+	@Test
+	public void GivenNeg3_GetRightAnswer() {
+		int num = -3;
+		
+		String binary = DecToBin.convert(num);
+		
+		assertEquals("11111111111111111111111111111101", binary);
+	}
+	
+	@Test
+	public void GivenMAX_GetRightAnswer() {
+		int num = Integer.MAX_VALUE;
+		
+		String binary = DecToBin.convert(num);
+		
+		assertEquals("1111111111111111111111111111111", binary);
+	}
+	
+	@Test
+	public void GivenMIN_GetRightAnswer() {
+		int num = Integer.MIN_VALUE;
+		
+		String binary = DecToBin.convert(num);
+		
+		assertEquals("10000000000000000000000000000000", binary);
+	}
+	
+	@Test
+	public void GivenZero_GetZero() {
+		int num = 0;
+		
+		String binary = DecToBin.convert(num);
+		
+		assertEquals("0", binary);
 	}
 
 }
