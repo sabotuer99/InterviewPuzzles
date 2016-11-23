10e53329c929d757dee79e3f704c66743f56a85d
sabotuer99 <sabotuer99@gmail.com>
2016-11-23 09:08:58 -0700

    implemented shuffle. One interesting tweak, if you get a rand between 0 and n every time, your distribution is biased. Between i and n, and it is fair... Fisher Yates suffle basically

--- InterviewPuzzles/src/puzzles/kennyyu/Shuffle.java
+++ InterviewPuzzles/src/puzzles/kennyyu/Shuffle.java
@@ -2,7 +2,20 @@ package puzzles.kennyyu;
 
 public class Shuffle {
 
-	
+	public static int[] shuffle(int[] arr){
+		if(arr == null){
+			return null;
+		}
+		
+		for(int i =0; i < arr.length; i++){
+			int k = randbetween(i, arr.length);
+			int t = arr[i];
+			arr[i] = arr[k];
+			arr[k] = t;
+		}
+		
+		return arr;
+	}
 	
 	
 	public static int randbetween(int a, int b){
--- InterviewPuzzles/src/spec/kennyyu/ShuffleTests.java
+++ InterviewPuzzles/src/spec/kennyyu/ShuffleTests.java
@@ -11,8 +11,37 @@ import puzzles.kennyyu.Shuffle;
 
 public class ShuffleTests {
 
+	@Test
+	public void Shuffle_10RandomDistributions() {
+		
+		for(int i = 0; i < 10; i++){
+			int[] arr = {0,1,2,3,4,5,6,7,8,9};
+			Shuffle.shuffle(arr);
+			System.out.println(Arrays.toString(arr));
+		}
+		
+		assertTrue(true);
+	}
 	
-	
+	@Test
+	public void Shuffle_1000000RandomDistributions() {
+		int[][] histogram = new int[10][10];
+		
+		for(int i = 0; i < 1000000; i++){
+			int[] arr = {0,1,2,3,4,5,6,7,8,9};
+			Shuffle.shuffle(arr);
+			
+			for(int j=0; j < 10; j++){
+				histogram[arr[j]][j]++;
+			}
+		}
+		
+		for(int j=0; j < 10; j++){
+			System.out.println(j + ": " + Arrays.toString(histogram[j]));
+		}
+		
+		assertTrue(true);
+	}
 	
 	
 	@Test
