5918879e818b3e5aa31e28243767f2cf85c9ee34
sabotuer99 <sabotuer99@gmail.com>
2016-11-23 13:25:15 -0700

    implemented DP solution, added test cases

--- InterviewPuzzles/src/puzzles/misc/HistoRectangle.java
+++ InterviewPuzzles/src/puzzles/misc/HistoRectangle.java
@@ -1,5 +1,8 @@
 package puzzles.misc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /************
  * 
  * Question from here: 
@@ -9,8 +12,36 @@ package puzzles.misc;
 public class HistoRectangle {
 
 	public static int maxRect(int[] histogram) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		if(histogram == null || histogram.length == 0){
+			return 0;
+		}
+		
+		List<Integer> D = new ArrayList<>();
+		
+		for(int i = 0; i < histogram[0]; i++){
+			D.add(1);
+		}
+		
+		int maxArea = histogram[0];
+		
+		for(int i = 1; i < histogram.length; i++){
+			List<Integer> tD = new ArrayList<>();
+			int colMax = 0;
+			for(int j = 0; j < histogram[i]; j++){
+				int area = j+1;
+				if(j < D.size()){
+					tD.add(D.get(j) + 1);
+					area = tD.get(j) * (j+1);				
+				} else {
+					tD.add(1);
+				}
+				colMax = Math.max(area, colMax);
+			}
+			maxArea = Math.max(maxArea, colMax);
+			
+			D = tD;
+		}
+		return maxArea;
 	}
-
 }
--- InterviewPuzzles/src/spec/misc/HistoRectangleTests.java
+++ InterviewPuzzles/src/spec/misc/HistoRectangleTests.java
@@ -16,5 +16,41 @@ public class HistoRectangleTests {
 		
 		assertEquals(14, max);
 	}
+	
+	@Test
+	public void VideoExample2_GetRightAnswer() {
+		int[] histogram = {1,3,5,3,0,2,6,6,1,0,3,6};
+		
+		int max = HistoRectangle.maxRect(histogram);
+		
+		assertEquals(12, max);
+	}
+	
+	@Test
+	public void GivenNull_Return0() {
+		int[] histogram = null;
+		
+		int max = HistoRectangle.maxRect(histogram);
+		
+		assertEquals(0, max);
+	}
+	
+	@Test
+	public void GivenEmpty_Return0() {
+		int[] histogram = {};
+		
+		int max = HistoRectangle.maxRect(histogram);
+		
+		assertEquals(0, max);
+	}
+	
+	@Test
+	public void GivenOneValue_ReturnSame() {
+		int[] histogram = {10};
+		
+		int max = HistoRectangle.maxRect(histogram);
+		
+		assertEquals(10, max);
+	}
 
 }
