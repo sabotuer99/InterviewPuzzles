f7f861d458eff35ef8d0fbfdacf55cc606bf5183
troy <sabotuer99@gmail.com>
2016-11-22 23:13:54 -0700

    Implemented formula parser

--- InterviewPuzzles/src/puzzles/cracking/derivative/Derivative.java
+++ InterviewPuzzles/src/puzzles/cracking/derivative/Derivative.java
@@ -1,12 +1,59 @@
 package puzzles.cracking.derivative;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Derivative {
 
 	public static Map<Integer,Integer> parse(String formula) {
-		// TODO Auto-generated method stub
-		return null;
+		String[] symbols = formula.split(" ");
+		String regex = "([-]?)(\\d*)(x?)(\\^\\d+)?";
+		Pattern pattern = Pattern.compile(regex);
+		int positive = 1;
+		Map<Integer,Integer> parsed = new HashMap<>();
+		
+		for(int i = 0; i < symbols.length; i++){
+			String symbol = symbols[i];
+			
+			if(0 == (i & 1)){
+				Matcher matcher = pattern.matcher(symbol);
+				matcher.find();
+				String sign = matcher.group(1);
+				String coef = matcher.group(2);
+				String var = matcher.group(3);
+				String exp = matcher.group(4);
+				
+				if("-".equals(sign)){
+					positive *= -1;
+				}
+				
+				if("".equals(coef) || coef == null){
+					coef = "1";
+				}
+				
+				if("".equals(var) || var == null){
+					exp = "0";
+				} else if ("".equals(exp) || exp == null){
+					exp = "1";
+				} else {
+					exp = exp.substring(1);
+				}
+				
+				Integer pcoef = positive * Integer.parseInt(coef);
+				Integer pexp = Integer.parseInt(exp);
+				parsed.put(pexp, pcoef);
+			} else {
+				if("+".equals(symbol)){
+					positive = 1;
+				} else {
+					positive = -1;
+				}
+			}
+		}
+		
+		return parsed;
 	}
 
 }
--- InterviewPuzzles/src/spec/cracking/derivative/DerivativeTests.java
+++ InterviewPuzzles/src/spec/cracking/derivative/DerivativeTests.java
@@ -16,14 +16,39 @@ public class DerivativeTests {
 	}
 
 	@Test
-	public void Parse_CorrectResult() {
+	public void ParseSample_CorrectResult() {
 		String formula = "3x^2 + 5x + 2";
 		
 		Map<Integer,Integer> parsed = Derivative.parse(formula);
 		
-		assertEquals(3, parsed.size());
-		assertTrue(parsed.containsKey(2));
+		assertEquals("Incorrect Size", 3, parsed.size());
+
 		assertEquals(3, (int)parsed.get(2));
+		assertEquals(5, (int)parsed.get(1));
+		assertEquals(2, (int)parsed.get(0));
+	}
+	
+	@Test
+	public void ParseSampleWithNeg_CorrectResult() {
+		String formula = "-3x^2 - 5x - -2";
+		
+		Map<Integer,Integer> parsed = Derivative.parse(formula);
+		
+		assertEquals("Incorrect Size", 3, parsed.size());
+
+		assertEquals(-3, (int)parsed.get(2));
+		assertEquals(-5, (int)parsed.get(1));
+		assertEquals(2, (int)parsed.get(0));
+	}
+	
+	@Test
+	public void ParseSuperSimple_CorrectResult() {
+		String formula = "x";
+		
+		Map<Integer,Integer> parsed = Derivative.parse(formula);
+		
+		assertEquals("Incorrect Size", 1, parsed.size());
+		assertEquals(1, (int)parsed.get(1));
 	}
 	
 }
