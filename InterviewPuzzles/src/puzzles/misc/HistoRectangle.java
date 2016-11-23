99af504dbabbb0b1f059b1ca9a3930d78a180b95
sabotuer99 <sabotuer99@gmail.com>
2016-11-23 14:08:39 -0700

    added stack implementation and a couple test cases for that

--- InterviewPuzzles/src/puzzles/misc/HistoRectangle.java
+++ InterviewPuzzles/src/puzzles/misc/HistoRectangle.java
@@ -1,6 +1,8 @@
 package puzzles.misc;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /************
@@ -11,6 +13,44 @@ import java.util.List;
 ************/
 public class HistoRectangle {
 
+	public static int stackRect(int[] histogram) {
+		
+		if(histogram == null || histogram.length == 0){
+			return 0;
+		}
+		
+		Deque<Integer> positions= new LinkedList<>();
+		Deque<Integer> heights = new LinkedList<>();
+		
+		positions.push(0);
+		heights.push(histogram[0]);
+		int maxarea = histogram[0];
+		
+		for(int i = 1; i < histogram.length; i++){
+			if(heights.peekFirst() != null && histogram[i] > heights.peekFirst()){
+				heights.push(histogram[i]);
+				positions.push(i);
+			} else if(heights.peekFirst() != null && histogram[i] < heights.peekFirst()) {
+				int lastpos = 0;
+				while(heights.peekFirst() != null && histogram[i] < heights.peekFirst()){
+					int h = heights.pop();
+					lastpos = positions.pop();
+					maxarea = Math.max(maxarea, h * (i-lastpos));
+				}
+				heights.push(histogram[i]);
+				positions.push(lastpos);
+			}
+		}
+		
+		while(heights.peek() != null){
+			int h = heights.pop();
+			int p = positions.pop();
+			maxarea = Math.max(maxarea, h * (histogram.length - p));
+		}
+		
+		return maxarea;
+	}
+	
 	public static int maxRect(int[] histogram) {
 		
 		if(histogram == null || histogram.length == 0){
--- InterviewPuzzles/src/spec/misc/HistoRectangleTests.java
+++ InterviewPuzzles/src/spec/misc/HistoRectangleTests.java
@@ -27,6 +27,24 @@ public class HistoRectangleTests {
 	}
 	
 	@Test
+	public void Stack_VideoExample_GetRightAnswer() {
+		int[] histogram = {1,3,5,3,2,2,3,3,1,0,3,6};
+		
+		int max = HistoRectangle.stackRect(histogram);
+		
+		assertEquals(14, max);
+	}
+	
+	@Test
+	public void Stack_VideoExample2_GetRightAnswer() {
+		int[] histogram = {1,3,5,3,0,2,6,6,1,0,3,6};
+		
+		int max = HistoRectangle.stackRect(histogram);
+		
+		assertEquals(12, max);
+	}
+	
+	@Test
 	public void GivenNull_Return0() {
 		int[] histogram = null;
 		
