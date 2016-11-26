77324563c2c6b18bfe32595c7afa4fa60693bc8f
troy <sabotuer99@gmail.com>
2016-11-26 11:52:19 -0700

    added code and tests for anagram check

--- InterviewPuzzles/src/puzzles/cracking/chapter1/AreAnagrams.java
+++ InterviewPuzzles/src/puzzles/cracking/chapter1/AreAnagrams.java
@@ -1,5 +1,35 @@
 package puzzles.cracking.chapter1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AreAnagrams {
+
+	public static boolean areAnagrams(String word1, String word2) {
+		
+		if(word1 == null || word2 == null || word1 == "" || word2 == "" 
+				|| word1.length() != word2.length()){
+			return false;
+		}
+			
+		word1 = word1.toUpperCase();
+		word2 = word2.toUpperCase();
+		Map<Character, Integer> letterCount = new HashMap<>();
+		
+		for(char letter : word1.toCharArray()){
+			Integer oldCount = letterCount.containsKey(letter) ? letterCount.get(letter) : 0;
+			letterCount.put(letter, oldCount + 1); 			
+		}
+		
+		for(char letter : word2.toCharArray()){
+			Integer oldCount = letterCount.get(letter);
+			if(oldCount == null || oldCount == 0){
+				return false;
+			}
+			letterCount.put(letter, oldCount - 1);
+		}
+		
+		return true;
+	}
 
 }
--- InterviewPuzzles/src/spec/cracking/chapter1/AreAnagramsTests.java
+++ InterviewPuzzles/src/spec/cracking/chapter1/AreAnagramsTests.java
@@ -4,11 +4,78 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import puzzles.cracking.chapter1.AreAnagrams;
+
 public class AreAnagramsTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void GivenRaceAndCare_ReturnTrue() {
+		String word1 = "Race";
+		String word2 = "Care";
+		
+		boolean test = AreAnagrams.areAnagrams(word1, word2);
+		
+		assertTrue(test);
+	}
+	
+	@Test
+	public void GivenRaceAndDare_ReturnFalse() {
+		String word1 = "Race";
+		String word2 = "Dare";
+		
+		boolean test = AreAnagrams.areAnagrams(word1, word2);
+		
+		assertFalse(test);
+	}
+	
+	@Test
+	public void GivenRaceAndRRRR_ReturnFalse() {
+		String word1 = "Race";
+		String word2 = "RRRR";
+		
+		boolean test = AreAnagrams.areAnagrams(word1, word2);
+		
+		assertFalse(test);
 	}
 
+	@Test
+	public void GivenRaceAndNull_ReturnFalse() {
+		String word1 = "Race";
+		String word2 = null;
+		
+		boolean test = AreAnagrams.areAnagrams(word1, word2);
+		
+		assertFalse(test);
+	}
+	
+	@Test
+	public void GivenNullAndWord_ReturnFalse() {
+		String word1 = null;
+		String word2 = "Word";
+		
+		boolean test = AreAnagrams.areAnagrams(word1, word2);
+		
+		assertFalse(test);
+	}
+	
+	@Test
+	public void GivenRaceAndEmptyString_ReturnFalse() {
+		String word1 = "Race";
+		String word2 = "";
+		
+		boolean test = AreAnagrams.areAnagrams(word1, word2);
+		
+		assertFalse(test);
+	}
+	
+	@Test
+	public void GivenEmptyStringAndWord_ReturnFalse() {
+		String word1 = "";
+		String word2 = "Word";
+		
+		boolean test = AreAnagrams.areAnagrams(word1, word2);
+		
+		assertFalse(test);
+	}
+	
 }
