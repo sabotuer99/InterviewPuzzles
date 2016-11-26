ddb5b2d941a9eb84573d49fd41b4debc41687e0d
troy <sabotuer99@gmail.com>
2016-11-26 14:11:50 -0700

    added some row column clobber problem 1.7

--- InterviewPuzzles/src/puzzles/cracking/chapter1/RectMatrix.java
+++ InterviewPuzzles/src/puzzles/cracking/chapter1/RectMatrix.java
@@ -2,4 +2,38 @@ package puzzles.cracking.chapter1;
 
 public class RectMatrix {
 
+	public static int[][] clobberRowCol(int[][] input){
+		if(input == null || input.length == 0){
+			return input;
+		}
+		
+		int rows = input.length;
+		int cols = input[0].length;
+		boolean[] deadcol = new boolean[cols];
+		int[][] result = new int[rows][cols];
+		
+		for(int row = 0; row < rows; row++){
+			boolean deadrow = false;
+			for(int col = 0; col < cols; col++){
+				if(deadrow || deadcol[col]){
+					result[row][col] = 0;
+					continue;
+				} else if(input[row][col] == 0){
+					deadrow = true;
+					deadcol[col] = true;
+					for(int i = 0; i <= col; i++){
+						result[row][i] = 0;
+					}
+					for(int j = 0; j < row; j++){
+						result[j][col] = 0;
+					}
+				} else {
+					result[row][col] = input[row][col];
+				}
+			}
+		}
+		
+		return result;
+	}
+	
 }
--- InterviewPuzzles/src/spec/cracking/chapter1/RectMatrixTests.java
+++ InterviewPuzzles/src/spec/cracking/chapter1/RectMatrixTests.java
@@ -2,13 +2,63 @@ package spec.cracking.chapter1;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+
+import puzzles.cracking.chapter1.RectMatrix;
 
 public class RectMatrixTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Given3x3WithZeroInMiddle() {
+		int[][] test = {{1,1,1},
+				        {1,0,1},
+				        {1,1,1}};
+		
+		int[][] expected = {{1,0,1},
+		        			{0,0,0},
+		        			{1,0,1}};
+		
+		int[][] result = RectMatrix.clobberRowCol(test);
+		
+		for(int i = 0; i < result.length; i++){
+			assertEquals(Arrays.toString(expected[i]), Arrays.toString(result[i]));
+		}
+	}
+	
+	@Test
+	public void Given3x4WithZeroInMiddle() {
+		int[][] test = {{1,1,1,1},
+				        {1,0,1,1},
+				        {1,1,1,1}};
+		
+		int[][] expected = {{1,0,1,1},
+		        			{0,0,0,0},
+		        			{1,0,1,1}};
+		
+		int[][] result = RectMatrix.clobberRowCol(test);
+		
+		for(int i = 0; i < result.length; i++){
+			assertEquals(Arrays.toString(expected[i]), Arrays.toString(result[i]));
+		}
+	}
+	
+	@Test
+	public void Given3x4WithMultipleZeros() {
+		int[][] test = {{0,1,1,1},
+				        {1,1,1,1},
+				        {1,1,1,0}};
+		
+		int[][] expected = {{0,0,0,0},
+		        			{0,1,1,0},
+		        			{0,0,0,0}};
+		
+		int[][] result = RectMatrix.clobberRowCol(test);
+		
+		for(int i = 0; i < result.length; i++){
+			assertEquals(Arrays.toString(expected[i]), Arrays.toString(result[i]));
+		}
 	}
 
 }
