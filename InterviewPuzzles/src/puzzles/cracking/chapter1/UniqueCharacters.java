7e161d11d571361719326eda9e3e4776ffeca1a0
troy <sabotuer99@gmail.com>
2016-11-25 21:06:48 -0700

    added tests cases and basic code structure

--- InterviewPuzzles/src/puzzles/cracking/chapter1/UniqueCharacters.java
+++ InterviewPuzzles/src/puzzles/cracking/chapter1/UniqueCharacters.java
@@ -2,4 +2,9 @@ package puzzles.cracking.chapter1;
 
 public class UniqueCharacters {
 
+	public static boolean uniqueChars(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
--- InterviewPuzzles/src/spec/cracking/chapter1/UniqueCharactersTests.java
+++ InterviewPuzzles/src/spec/cracking/chapter1/UniqueCharactersTests.java
@@ -1,14 +1,28 @@
 package spec.cracking.chapter1;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+import puzzles.cracking.chapter1.UniqueCharacters;
 
 public class UniqueCharactersTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void GivenWORD_ReturnTrue() {
+		String word = "WORD";
+		
+		boolean result = UniqueCharacters.uniqueChars(word);
+		
+		assertTrue(result);
+	}
+	
+	@Test
+	public void GivenBLAAAAH_ReturnFalse() {
+		String word = "BLAAAAH";
+		
+		boolean result = UniqueCharacters.uniqueChars(word);
+		
+		assertFalse(result);
 	}
 
 }
