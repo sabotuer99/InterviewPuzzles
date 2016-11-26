c0ebf62e08a6b11e1b6663ee2c4d261190961c7f
troy <sabotuer99@gmail.com>
2016-11-26 13:24:36 -0700

    added files for matrix row col deletion 17

---
+++ InterviewPuzzles/src/spec/cracking/chapter1/RectMatrixTests.java
@@ -0,0 +1,14 @@
+package spec.cracking.chapter1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RectMatrixTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
