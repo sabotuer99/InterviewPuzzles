07278947b76c5135aacbfda1d1923776b39a0253
troy <sabotuer99@gmail.com>
2016-11-25 21:27:07 -0700

    implemented code with and without using Set

--- InterviewPuzzles/src/puzzles/cracking/chapter1/UniqueCharacters.java
+++ InterviewPuzzles/src/puzzles/cracking/chapter1/UniqueCharacters.java
@@ -1,10 +1,46 @@
 package puzzles.cracking.chapter1;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UniqueCharacters {
 
+	
+	
+	
 	public static boolean uniqueChars(String word) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		if(word == null || word.length() == 0){
+			return false;
+		}
+		Set<Character> lettersSeen = new HashSet<>();
+		
+		for(char letter : word.toCharArray()){
+			if(lettersSeen.contains(letter)){
+				return false;
+			}
+			lettersSeen.add(letter);
+		}
+		return true;
+	}
+
+	public static boolean uniqueCharsNoSet(String word) {
+		if(word == null || word.length() == 0){
+			return false;
+		}
+		
+		char[] letters = word.toCharArray();
+		Arrays.sort(letters);
+		
+		for(int i = 1; i < letters.length; i++){
+			if(letters[i] == letters[i-1]){
+				return false;
+			}
+		}
+		return true;
+		
 	}
 
 }
--- InterviewPuzzles/src/spec/cracking/chapter1/UniqueCharactersTests.java
+++ InterviewPuzzles/src/spec/cracking/chapter1/UniqueCharactersTests.java
@@ -24,5 +24,59 @@ public class UniqueCharactersTests {
 		
 		assertFalse(result);
 	}
+	
+	@Test
+	public void GivenNull_ReturnFalse() {
+		String word = null;
+		
+		boolean result = UniqueCharacters.uniqueChars(word);
+		
+		assertFalse(result);
+	}
+	
+	@Test
+	public void GivenEmptyString_ReturnFalse() {
+		String word = "";
+		
+		boolean result = UniqueCharacters.uniqueChars(word);
+		
+		assertFalse(result);
+	}
+	
+	@Test
+	public void NoSet_GivenWORD_ReturnTrue() {
+		String word = "WORD";
+		
+		boolean result = UniqueCharacters.uniqueCharsNoSet(word);
+		
+		assertTrue(result);
+	}
+	
+	@Test
+	public void NoSet_GivenBLAAAAH_ReturnFalse() {
+		String word = "BLAAAAH";
+		
+		boolean result = UniqueCharacters.uniqueCharsNoSet(word);
+		
+		assertFalse(result);
+	}
+	
+	@Test
+	public void NoSet_GivenNull_ReturnFalse() {
+		String word = null;
+		
+		boolean result = UniqueCharacters.uniqueCharsNoSet(word);
+		
+		assertFalse(result);
+	}
+	
+	@Test
+	public void NoSet_GivenEmptyString_ReturnFalse() {
+		String word = "";
+		
+		boolean result = UniqueCharacters.uniqueCharsNoSet(word);
+		
+		assertFalse(result);
+	}
 
 }
