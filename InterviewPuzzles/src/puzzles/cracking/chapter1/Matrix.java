65bbc77ef7fb9fd99e974ca0177532b5b0a7eac5
troy <sabotuer99@gmail.com>
2016-11-26 13:09:17 -0700

    finished matrix rotation problem 1.6

--- InterviewPuzzles/src/puzzles/cracking/chapter1/Matrix.java
+++ InterviewPuzzles/src/puzzles/cracking/chapter1/Matrix.java
@@ -2,4 +2,35 @@ package puzzles.cracking.chapter1;
 
 public class Matrix {
 
+	private int[][] values;
+	private int N;
+	
+	public Matrix(int[][] values){
+		this.values = values;
+		this.N = values.length;
+		//could do some more validation here to ensure values are right size
+	}
+	
+	public int[][] rotateClockwise(){
+		
+		//iterate through rings
+		for(int ring = 0; ring < N/2; ring++){
+			int L = ring;
+			int R = N - 1 - ring;
+			//iterate along edge
+			for(int i = 0; i < R - L; i++){
+				int NW = values[L][L+i];
+				int NE = values[L+i][R];
+				int SE = values[R][R-i];
+				int SW = values[R-i][L];
+				//SW=>NW, NW=>NE, NE=>SE, SE=>SW
+				values[L][L+i] = SW;
+				values[L+i][R] = NW;
+				values[R][R-i] = NE;
+				values[R-i][L] = SE;
+			}
+		}
+		
+		return values;
+	}
 }
--- InterviewPuzzles/src/spec/cracking/chapter1/MatrixTests.java
+++ InterviewPuzzles/src/spec/cracking/chapter1/MatrixTests.java
@@ -2,13 +2,32 @@ package spec.cracking.chapter1;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+
+import puzzles.cracking.chapter1.Matrix;
 
 public class MatrixTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Given4x4Matrix_CorrectRotation() {
+		int[][] matrix = {{10,11,12,13},{20,21,22,23},{30,31,32,33},{40,41,42,43}};
+		
+		Matrix sut = new Matrix(matrix);
+		
+		int[][] rotated = sut.rotateClockwise();
+			
+		int[][] expected = {{40,30,20,10},{41,31,21,11},{42,32,22,12},{43,33,23,13}};
+		
+		for(int i = 0; i < 4; i++){
+			System.out.println(Arrays.toString(rotated[i]));
+		}
+		
+		assertEquals(Arrays.toString(expected[0]), Arrays.toString(rotated[0]));
+		assertEquals(Arrays.toString(expected[1]), Arrays.toString(rotated[1]));
+		assertEquals(Arrays.toString(expected[2]), Arrays.toString(rotated[2]));
+		assertEquals(Arrays.toString(expected[3]), Arrays.toString(rotated[3]));
 	}
 
 }
