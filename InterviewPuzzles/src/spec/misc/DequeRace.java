2fa446ee7fe794c0f32fd834f583cf7c2adf814e
sabotuer99 <sabotuer99@gmail.com>
2016-11-28 13:34:02 -0700

    raced a LinkedList and ArrayDeque... LL lost

---
+++ InterviewPuzzles/src/spec/misc/DequeRace.java
@@ -0,0 +1,61 @@
+package spec.misc;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayDeque;
+import java.util.Date;
+import java.util.Deque;
+import java.util.LinkedList;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+import org.junit.Test;
+
+public class DequeRace {
+
+	int n = 5000000;
+	
+	@Test
+	public void Race_Reused() {
+		
+		System.out.println("Reused Queues");
+		System.out.println("********************");
+		
+		Deque<Integer> sutAD = new ArrayDeque<>(n);
+		Deque<Integer> sutLL = new LinkedList<>();
+		
+		for(int i = 0; i < 10; i++){
+			runs(n, sutAD, "ArrayDeque");
+			runs(n, sutLL, "LinkedList");
+		}
+	}
+	
+	@Test
+	public void Race_New() {
+		
+		System.out.println("New Queues Every Run");
+		System.out.println("********************");
+				
+		for(int i = 0; i < 10; i++){
+			Deque<Integer> sutAD = new ArrayDeque<>(n);
+			Deque<Integer> sutLL = new LinkedList<>();
+			runs(n, sutAD, "ArrayDeque");
+			runs(n, sutLL, "LinkedList");
+		}
+	}
+
+	
+	private static void runs(int n, Deque<Integer> list, String listType){
+		long start = System.currentTimeMillis();
+		for(int i = 0; i < n; i++){
+			list.push(i);
+		}
+		
+		for(int i = 0; i < n; i++){
+			list.pop();
+		}
+		long now = System.currentTimeMillis();
+		System.out.println(listType + ": " + (now - start));
+	}
+
+}
