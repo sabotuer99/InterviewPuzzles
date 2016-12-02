19a79f945e7e8944413daca52408aa177f5133e7
sabotuer99 <sabotuer99@gmail.com>
2016-12-02 11:29:03 -0700

    thinking of making this multithread capable wuuuuut!

--- InterviewPuzzles/src/puzzles/cracking/chapter7/callcenter/CallCenter.java
+++ InterviewPuzzles/src/puzzles/cracking/chapter7/callcenter/CallCenter.java
@@ -2,14 +2,52 @@ package puzzles.cracking.chapter7.callcenter;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import puzzles.cracking.chapter7.callcenter.events.CallEvent;
+import puzzles.cracking.chapter7.callcenter.events.CallEventHandler;
 
 public class CallCenter {
 
-	Deque<Employee> availableRespondends = new ArrayDeque<>();
-	Deque<Employee> availableManagers = new ArrayDeque<>();
-	Deque<Employee> availableDirectors = new ArrayDeque<>();
+	Deque<Employee> availableRespondends = new ConcurrentLinkedDeque<>();
+	Deque<Employee> availableManagers = new ConcurrentLinkedDeque<>();
+	Deque<Employee> availableDirectors = new ConcurrentLinkedDeque<>();
 	
-	public void addRespondend(Employee employee){
+	Deque<Call> waitingRespondent = new ConcurrentLinkedDeque<>();
+	Deque<Call> waitingManager = new ConcurrentLinkedDeque<>();
+	Deque<Call> waitingDirector = new ConcurrentLinkedDeque<>();
+	
+	CallEventHandler escToManagerHandler = new CallEventHandler(){
+		@Override
+		public void handle(CallEvent e) {
+			if(availableManagers.size() == 0){
+				if(availableDirectors.size() == 0){
+					waitingManager.offer(e.getCall());
+				} else {
+					Employee director = availableDirectors.poll();
+					assignToCall(director, e.getCall());
+				}
+			} else {
+				Employee manager = availableManagers.poll();
+				assignToCall(manager, e.getCall());
+			}	
+		}
+	};
+	
+	CallEventHandler escToDirectorHandler = new CallEventHandler(){
+		@Override
+		public void handle(CallEvent e) {			
+			if(availableDirectors.size() == 0){
+				waitingManager.offer(e.getCall());
+			} else {
+				Employee director = availableDirectors.poll();
+				assignToCall(director, e.getCall());
+			}
+		}
+	};
+	
+	
+	public void addRespondent(Employee employee){
 		
 	}
 	
@@ -18,6 +56,10 @@ public class CallCenter {
 	}
 	
 	public void addDirector(Employee employee){
+		
+	}
+	
+	public void assignToCall(Employee employee, Call call){
 		
 	}
 }
