da7212c6db196ecf57911a09b04966472468476d
sabotuer99 <sabotuer99@gmail.com>
2016-12-06 14:13:34 -0700

    added test scaffolding for remaining state classes

---
+++ InterviewPuzzles/src/spec/cracking/chapter7/jukebox/state/PartialPaidStateTests.java
@@ -0,0 +1,33 @@
+package spec.cracking.chapter7.jukebox.state;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class PartialPaidStateTests extends BaseTest {
+
+	@Test
+	public void InsertCoin_(){
+		fail();
+	}
+	
+	@Test
+	public void InsertRecord_(){
+		fail();
+	}
+	
+	@Test
+	public void MakeSelection_(){
+		fail();
+	}
+	
+	@Test
+	public void RemoveRecord_(){
+		fail();
+	}
+	
+	@Test
+	public void ReturnCoins_(){
+		fail();
+	}
+}
---
+++ InterviewPuzzles/src/spec/cracking/chapter7/jukebox/state/ReadyStateTests.java
@@ -0,0 +1,32 @@
+package spec.cracking.chapter7.jukebox.state;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ReadyStateTests extends BaseTest  {
+	@Test
+	public void InsertCoin_(){
+		fail();
+	}
+	
+	@Test
+	public void InsertRecord_(){
+		fail();
+	}
+	
+	@Test
+	public void MakeSelection_(){
+		fail();
+	}
+	
+	@Test
+	public void RemoveRecord_(){
+		fail();
+	}
+	
+	@Test
+	public void ReturnCoins_(){
+		fail();
+	}
+}
