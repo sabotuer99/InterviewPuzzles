e56b81080a399d48e5e3832dc67568e861781952
sabotuer99 <sabotuer99@gmail.com>
2016-12-06 15:57:24 -0700

    tests for Partial and Loaded states

--- InterviewPuzzles/src/spec/cracking/chapter7/jukebox/state/LoadedStateTests.java
+++ InterviewPuzzles/src/spec/cracking/chapter7/jukebox/state/LoadedStateTests.java
@@ -4,30 +4,71 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import puzzles.cracking.chapter7.jukebox.state.State;
+import puzzles.cracking.chapter7.jukebox.state.UnloadedState;
+
 public class LoadedStateTests extends BaseTest {
 
 	@Test
-	public void InsertCoin_(){
-		fail();
+	public void InsertCoin_GetPartialState(){
+		State sut = getSut();
+		
+		State next = sut.insertCoin(quarter());
+		
+		assertEquals("Partially Paid", next.status());
+		assertEquals(25, next.getCoinBalance());
+	}
+
+	@Test
+	public void InsertRecord_StillLoadedState(){
+		State sut = getSut();
+		
+		State next = sut.insertRecord(testRecord());
+		
+		assertEquals("Loaded", next.status());
+		assertEquals(2, next.getAvailableSongs().size());
 	}
 	
 	@Test
-	public void InsertRecord_(){
-		fail();
+	public void MakeSelection_DoesNothing(){
+		State sut = getSut();
+		
+		State next = sut.makeSelection(0);
+		
+		assertEquals("Loaded", next.status());
 	}
 	
 	@Test
-	public void MakeSelection_(){
-		fail();
+	public void ReturnCoins_DoesNothing(){
+		State sut = getSut();
+		
+		State next = sut.returnCoins();
+		
+		assertEquals("Loaded", next.status());
 	}
 	
 	@Test
-	public void RemoveRecord_(){
-		fail();
+	public void RemoveRecord_RecordsEmpty_BecomesUnloaded(){
+		State sut = getSut();
+		
+		State next = sut.removeRecord(0);
+		
+		assertEquals("Unloaded", next.status());
 	}
 	
 	@Test
-	public void ReturnCoins_(){
-		fail();
+	public void RemoveRecord_RecordsNotEmpty_StillLoaded(){
+		State sut = getSut();
+		
+		State next = sut.insertRecord(testRecord());
+		next = next.removeRecord(0);
+		
+		assertEquals("Loaded", next.status());
+	}
+	
+	private State getSut() {
+		State sut = new UnloadedState(75).insertRecord(testRecord());
+		assertEquals("Loaded", sut.status());
+		return sut;
 	}
 }
--- InterviewPuzzles/src/spec/cracking/chapter7/jukebox/state/PartialPaidStateTests.java
+++ InterviewPuzzles/src/spec/cracking/chapter7/jukebox/state/PartialPaidStateTests.java
@@ -1,33 +1,78 @@
 package spec.cracking.chapter7.jukebox.state;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.Test;
+
+import puzzles.cracking.chapter7.jukebox.Coin;
+import puzzles.cracking.chapter7.jukebox.state.State;
+import puzzles.cracking.chapter7.jukebox.state.UnloadedState;
 
 public class PartialPaidStateTests extends BaseTest {
 
 	@Test
-	public void InsertCoin_(){
-		fail();
+	public void InsertCoin_StillNotEnough_StaysPartial(){
+		State sut = getSut();
+		
+		State next = sut.insertCoin(quarter());
+		
+		assertEquals("Partially Paid", next.status());
 	}
 	
 	@Test
-	public void InsertRecord_(){
-		fail();
+	public void InsertRecord_StillLoadedState(){
+		State sut = getSut();
+		
+		State next = sut.insertRecord(testRecord());
+		
+		assertEquals("Partially Paid", next.status());
+		assertEquals(2, next.getAvailableSongs().size());
 	}
 	
 	@Test
-	public void MakeSelection_(){
-		fail();
+	public void MakeSelection_DoesNothing(){
+		State sut = getSut();
+		
+		State next = sut.makeSelection(0);
+		
+		assertEquals("Partially Paid", next.status());
 	}
 	
 	@Test
-	public void RemoveRecord_(){
-		fail();
+	public void RemoveRecord_RecordsEmpty_BecomesUnloadedRefundsCoins(){
+		State sut = getSut();
+		
+		State next = sut.removeRecord(0);
+		List<Coin> coins = next.emptyCoinReturn();
+		
+		assertEquals("Unloaded", next.status());
+		assertEquals(1, coins.size());
 	}
 	
 	@Test
-	public void ReturnCoins_(){
-		fail();
+	public void RemoveRecord_RecordsNotEmpty_StillLoaded(){
+		State sut = getSut();
+		
+		State next = sut.insertRecord(testRecord());
+		next = next.removeRecord(0);
+		
+		assertEquals("Partially Paid", next.status());
+	}
+	
+	@Test
+	public void ReturnCoins_BecomesLoaded(){
+		State sut = getSut();
+		
+		State next = sut.returnCoins();
+		
+		assertEquals("Loaded", next.status());
+	}
+	
+	private State getSut() {
+		State sut = new UnloadedState(75).insertRecord(testRecord()).insertCoin(quarter());
+		assertEquals("Partially Paid", sut.status());
+		return sut;
 	}
 }
