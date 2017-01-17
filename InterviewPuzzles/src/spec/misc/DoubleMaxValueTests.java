ccef0549dbc02ffad1ba29aba003ddd9ec35f79f
sabotuer99 <sabotuer99@gmail.com>
2017-01-17 15:46:27 -0700

    curious what happens when you exceed Double.MAX_VALUE (spoiler: it's infinity...)

---
+++ InterviewPuzzles/src/spec/misc/DoubleMaxValueTests.java
@@ -0,0 +1,12 @@
+package spec.misc;
+
+import org.junit.Test;
+
+public class DoubleMaxValueTests {
+
+	@Test
+	public void PrintDoubleMax(){
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MAX_VALUE * 2.0);
+	}
+}
